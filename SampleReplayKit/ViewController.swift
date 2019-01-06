214c293b289ea7f432eca1e3876f892b09d4f8fe
daisukenagata <dbank0208@gmail.com>
2019-01-06 09:00:22 +0900

    Initial Commit

---
+++ SampleReplayKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleReplayKit
+//
+//  Created by 永田大祐 on 2019/01/06.
+//  Copyright © 2019 永田大祐. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
